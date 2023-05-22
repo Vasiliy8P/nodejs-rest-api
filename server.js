c2e08599788a0a4874252ada854ad7038411e231
Vasiliy Pshenychniuk <vasiliy8@gmail.com>
2023-05-23 00:02:24 +0300

    init progect

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require("mongoose");
+
+// 2WG9WNzzmb5dbCHL
+
+const DB_HOST = "mongodb+srv://Vasiliy:2WG9WNzzmb5dbCHL@cluster0.zxzunjg.mongodb.net/contacts_list?retryWrites=true&w=majority";
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    console.log("Database connect success");
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000")
+    });
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+
