d96634041ce90a60193dc523b1baef47009c151b
Vasiliy Pshenychniuk <vasiliy8@gmail.com>
2023-05-26 23:59:47 +0300

    fix .env

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const app = require('./app')
 const mongoose = require('mongoose');
 
-// tKDMd6CGMNjM4dpO
-
-// DB_HOST = "mongodb+srv://vasiliy8P:tKDMd6CGMNjM4dpO@cluster0.kxphkog.mongodb.net/db-contacts?retryWrites=true&w=majority"
-
 const {DB_HOST, PORT = 3000} = process.env
 
 mongoose.connect(DB_HOST)
