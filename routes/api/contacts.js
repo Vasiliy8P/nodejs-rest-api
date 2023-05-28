513aa5a3b900d20b623f4b6326452de1aeefa4bf
Vasiliy Pshenychniuk <vasiliy8@gmail.com>
2023-05-28 15:49:07 +0300

    add contactSchema

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const contactsController = require("../../controllers/contactsController");
 const { validateBody } = require("../../decorators");
-const { verificateBody } = require("../../decorators");
+// const { verificateBody } = require("../../decorators");
 const schemas = require("../../schemas/contactsSchemas");
 
 const router = express.Router();
 
 router.get('/', contactsController.listContacts);
 
-router.get('/:contactId', contactsController.getContactById);
+// router.get('/:contactId', contactsController.getContactById);
 
 router.post('/', validateBody(schemas.addContactSchema), contactsController.addContact);
 
-router.delete('/:contactId', contactsController.removeContact);
+// router.delete('/:contactId', contactsController.removeContact);
 
-router.put('/:contactId', verificateBody(), validateBody(schemas.addContactSchema), contactsController.updateContactById);
+// router.put('/:contactId', verificateBody(), validateBody(schemas.addContactSchema), contactsController.updateContactById);
 
 module.exports = router;
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -4,6 +4,8 @@ const addContactSchema = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().required(),
     phone: Joi.string().required(),
+    addres: Joi.string(),
+    favorite: Joi.boolean(),
 });
 
 module.exports = {
